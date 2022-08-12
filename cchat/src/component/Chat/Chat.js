0ec78f09cb817d5745c0bb622d08a8a045c47f3a
Kritika1-art <71672765+Kritika1-art@users.noreply.github.com>
2022-08-12 11:02:18 +0530

    Update Chat.js

--- cchat/src/component/Chat/Chat.js
+++ cchat/src/component/Chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "https://demo-cchat.herokuapp.com/";
+const ENDPOINT = "";
 
 const Chat = () => {
     const [id, setid] = useState("");
